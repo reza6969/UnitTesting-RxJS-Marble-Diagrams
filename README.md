e05f45f145ee0ef3ed08a3572d7d6391a4d428f1
r.soltani <infooffice1399@gmail.com>
2023-07-27 21:25:51 +0330

    030-Demo Cold Observable Marble Diagrams
    031-Introduction Marble Testing Hot and Cold Observables
    032-Understanding Frames in Jasmine Marbles
    033-Marble Testing for Cold Observable
    034-Demo Marble Testing Cold Observable Part 1
    035-Demo Marble Testing Cold Observable Part 2

--- README.md
+++ README.md
@@ -24,3 +24,8 @@
 ## 🙏 This Git Repostiory status is uptodate
 
 I am trying my best to keep this repository live and working. I recently updated this repository to match with latest angular version. Also did a quick test everything looks okay. Still if you find any issues please feel free to create issues or post a comment to UnitTesting-RxJS-Marble-Diagrams Problem Solver article.
+
+cheetSheet
+## npm i -d jasmine-marbles
+## node D:\Projects\GitHub\UnitTesting-RxJS-Marble-Diagrams\src\hot-cold\cold.js
+## npm test
--- src/hot-cold/cold.spec.ts
+++ src/hot-cold/cold.spec.ts
@@ -1,5 +1,5 @@
 import { cold, getTestScheduler } from 'jasmine-marbles';
-import { of, Observable, throwError, from } from 'rxjs';
+import { of, Observable, throwError, from, EMPTY } from 'rxjs';
 
 describe('COLD', () => {
   it('of with one value', () => {
@@ -16,4 +16,56 @@ describe('COLD', () => {
 
   it('should trim the spaces', () => {
   });
+  it('can searched an alphabet', () => {
+    const provided = search('e');
+    const expected = cold('(e|)');
+    expect(provided).toBeObservable(expected);
+  });
+
+  it('can return empty when no value found', ()=>{
+    const provided = search('E');
+    const expected = cold('|');
+    expect(provided).toBeObservable(expected);
+  });
+
+  it('can search vowels', ()=>{
+    const provided = searchVowels();
+    const expected = cold('(aeiou|)');
+    expect(provided).toBeObservable(expected);
+  });
 });
+
+const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+];
+
+function search(val) {
+  return alphabet.indexOf(val) > -1 ? of(`${val}`): EMPTY;
+}
+
+function searchVowels() {
+  const vowels = alphabet.filter( s =>
+    ['a', 'e', 'i', 'o', 'u'].indexOf(s) > -1 ? true : false
+  );
+  return from(vowels);
+}
