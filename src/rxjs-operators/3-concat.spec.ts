68a938ddc9a95e3fba1eefdf94df96e905b2b0a5
r.soltani <infooffice1399@gmail.com>
2023-08-15 14:24:11 +0330

    Demo Marble Testing Subscription Behavior of Concat Operator

--- src/rxjs-operators/3-concat.spec.ts
+++ src/rxjs-operators/3-concat.spec.ts
@@ -26,13 +26,16 @@ describe('concat operator', () => {
 
   it('should concat 2 observables', () => {
     const $obs1 = cold('---a---b|');
-
+    const sub1 =        '^-------!';
     const $obs2 = cold('---c---d|');
-
+    // const sub2 =  '        ^-------!';
+    const sub2 =       '--------^-------!';
     const $result = $obs1.pipe(concat($obs2));
 
     const $expected = cold('---a---b---c---d|');
 
     expect($result).toBeObservable($expected);
+    expect($obs1).toHaveSubscriptions(sub1);
+    expect($obs2).toHaveSubscriptions(sub2);
   });
 });
